[f] add correct normalization

dataset.py
@@ -1,3 +1,4 @@
+from skimage.util.dtype import img_as_bool
 from torch.utils.data import Dataset, DataLoader
 import torch
 from torch import nn, optim
@@ -18,7 +19,8 @@ class ColorizationDataset(Dataset):
             ])
         elif split == 'val':
             self.transforms = transforms.Resize((SIZE, SIZE),  Image.BICUBIC)
-        
+
+        self.max, self.min = self.__norm_param__()
         self.split = split
         self.size = SIZE
         self.paths = paths
@@ -42,6 +44,28 @@ class ColorizationDataset(Dataset):
     
     def __len__(self):
         return len(self.paths)
+
+    def __norm_param__(self):
+        img = np.mgrid[0:256, 0:256, 0:256].astype(np.float32)
+        img /= 255
+        min_param = dict()
+        max_param = dict()
+
+        img_lab = rgb2lab(img.T)
+
+        img_L = img_lab[:,:,:,0]
+        img_a = img_lab[:,:,:,1]
+        img_b = img_lab[:,:,:,2]
+
+        min_param['L'] = np.min(img_L)
+        min_param['a'] = np.min(img_a)
+        min_param['b'] = np.min(img_b)
+
+        max_param['L'] = np.max(img_L)
+        max_param['a'] = np.max(img_a)
+        max_param['b'] = np.max(img_b)
+
+        return max_param, min_param
 
 def make_dataloaders(batch_size=4, n_workers=4, pin_memory=True, **kwargs): # A handy function to make our dataloaders
     dataset = ColorizationDataset(**kwargs)

main.py
@@ -17,12 +17,12 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 from dataset import*
 
-paths = glob.glob("/home/liya/projects/ColorPix2Pix/archive/coco2017/train/*.jpg") # Your path for your dataset
+paths = glob.glob("/home/liya/devel/to_png/img_jpg/*.png") # Your path for your dataset
 np.random.seed(123)
-paths_subset = np.random.choice(paths, 115000, replace=False) # choosing 1000 images randomly
-rand_idxs = np.random.permutation(115000)
-train_idxs = rand_idxs[:114000] # choosing the first 8000 as training set
-val_idxs = rand_idxs[114000:] # choosing last 2000 as validation set
+paths_subset = np.random.choice(paths, 20, replace=False) # choosing 1000 images randomly
+rand_idxs = np.random.permutation(20)
+train_idxs = rand_idxs[:20] # choosing the first 8000 as training set
+val_idxs = rand_idxs[20:] # choosing last 2000 as validation set
 train_paths = paths_subset[train_idxs]
 val_paths = paths_subset[val_idxs]
 print(len(train_paths), len(val_paths))
@@ -65,9 +65,9 @@ def train_model(model, train_dl, epochs, display_every=1):
                 print(1)
                 # model.save()
 
-gen = siggraph17(True)
+gen = siggraph17(False)
 model = MainModel(gen)
-model.load()
+# model.load()
 train_model(model, train_dl, 10)
 model.save()
 